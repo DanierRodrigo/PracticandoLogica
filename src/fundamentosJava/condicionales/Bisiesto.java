d2e62ea01611f45d9c6b681babec17f083a1b79e
drodrigor <danierrodrigo@gmail.com>
2025-03-12 10:57:35 +0100

    Pide un año e indica si es bisiesto.

---
+++ src/fundamentosJava/condicionales/Bisiesto.java
@@ -0,0 +1,8 @@
+package fundamentosJava.condicionales;
+
+public class Bisiesto {
+
+    //Pide un año e indica si es bisiesto. Un año es bisiesto si:
+    //Es divisible por 4.
+    //No es divisible por 100, a menos que también sea divisible por 400.
+}
