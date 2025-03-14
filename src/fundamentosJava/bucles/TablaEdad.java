c7d02e1f5584993a2e7c9bfe8d91dfd3f27d1c48
drodrigor <danierrodrigo@gmail.com>
2025-03-14 10:47:20 +0100

    Pide el año de nacimiento de una persona. Pregunta hasta qué año quiere calcular su edad. Usa un bucle para mostrar la edad en cada año hasta el elegido.

---
+++ src/fundamentosJava/bucles/TablaEdad.java
@@ -0,0 +1,8 @@
+package fundamentosJava.bucles;
+
+public class TablaEdad {
+
+    //Pide el año de nacimiento de una persona.
+    //Pregunta hasta qué año quiere calcular su edad.
+    //Usa un bucle para mostrar la edad en cada año hasta el elegido.
+}
