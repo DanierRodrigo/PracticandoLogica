905c0998e24c6b3b56b3b6d635425516312e2c1d
drodrigor <danierrodrigo@gmail.com>
2025-03-14 11:23:41 +0100

    Pide al usuario cuánto quiere ahorrar cada mes. Pregunta cuántos meses quiere ahorrar. Calcula el total ahorrado mes a mes e incluye un interés del 2% mensual acumulativo.

---
+++ src/fundamentosJava/bucles/AhorroBancario.java
@@ -0,0 +1,8 @@
+package fundamentosJava.bucles;
+
+public class AhorroBancario {
+
+    //Pide al usuario cuánto quiere ahorrar cada mes.
+    //Pregunta cuántos meses quiere ahorrar.
+    //Calcula el total ahorrado mes a mes e incluye un interés del 2% mensual acumulativo.
+}
