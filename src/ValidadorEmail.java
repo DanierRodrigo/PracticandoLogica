a07f46f4fb8dd73e4e308c5c4e538e9d151f9a4e
drodrigor <danierrodrigo@gmail.com>
2025-03-20 18:05:19 +0100

    Crea un programa que valide si una dirección de correo electrónico ingresada es válida. Considera que un correo electrónico válido debe tener la forma: usuario@dominio.com

---
+++ src/ValidadorEmail.java
@@ -0,0 +1,5 @@
+public class ValidadorEmail {
+
+    //Crea un programa que valide si una dirección de correo electrónico ingresada es válida.
+    // Considera que un correo electrónico válido debe tener la forma: usuario@dominio.com
+}
