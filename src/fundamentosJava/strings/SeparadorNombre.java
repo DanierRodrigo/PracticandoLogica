6bb024262de4d6a5c5540926b81669f71142083d
drodrigor <danierrodrigo@gmail.com>
2025-03-20 18:37:34 +0100

    Se ingresa un nombre completo. Se separa en nombre(s) y apellido(s).

---
+++ src/fundamentosJava/strings/SeparadorNombre.java
@@ -0,0 +1,7 @@
+package fundamentosJava.strings;
+
+public class SeparadorNombre {
+
+    //Se ingresa un nombre completo. Se separa en nombre(s) y apellido(s).
+    //Si solo tiene un nombre, se muestra como único.
+}
