5121f0778a6e91505ebf3da653e8d6378a5dde33
drodrigor <danierrodrigo@gmail.com>
2025-03-28 14:50:03 +0100

    Crea un programa que simule una votación donde los usuarios puedan votar por diferentes candidatos. El programa debe mostrar los resultados al final.

---
+++ src/estructurasDeControl/contadorVotos.java
@@ -0,0 +1,7 @@
+package estructurasDeControl;
+
+public class contadorVotos {
+
+    //Crea un programa que simule una votación donde los usuarios puedan votar por diferentes candidatos.
+    // El programa debe mostrar los resultados al final.
+}
