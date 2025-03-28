2c7177dc476f8ae79e14b0b7b297f5912ead4610
drodrigor <danierrodrigo@gmail.com>
2025-03-28 14:28:12 +0100

    Implementa un programa que calcule el descuento aplicado a un producto, dependiendo de su precio y de si el cliente es miembro de un programa de fidelización.

---
+++ src/estructurasDeControl/calculadoraDescuento.java
@@ -0,0 +1,7 @@
+package estructurasDeControl;
+
+public class calculadoraDescuento {
+
+    //Implementa un programa que calcule el descuento aplicado a un producto,
+    //dependiendo de su precio y de si el cliente es miembro de un programa de fidelización.
+}
