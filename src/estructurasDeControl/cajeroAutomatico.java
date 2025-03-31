f80debe09c008ccf7856a72c831b8c8cd421027e
drodrigor <danierrodrigo@gmail.com>
2025-03-31 14:03:39 +0200

    Crea un programa que simule un cajero automático, permitiendo realizar las siguientes acciones: ingresar saldo, retirar dinero, consultar saldo y salir.

---
+++ src/estructurasDeControl/cajeroAutomatico.java
@@ -0,0 +1,7 @@
+package estructurasDeControl;
+
+public class cajeroAutomatico {
+
+    //Crea un programa que simule un cajero automático
+    //Permitiendo realizar las siguientes acciones: ingresar saldo, retirar dinero, consultar saldo y salir.
+}
