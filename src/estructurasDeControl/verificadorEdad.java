144bf1eaa0091b8d49f232c689492c98a9a33c09
drodrigor <danierrodrigo@gmail.com>
2025-04-01 17:50:35 +0200

    Crea un programa que verifique si una persona es mayor de edad. La persona debe ingresar su edad, y el programa debe imprimir si es mayor de edad (18 años o más) o menor de edad.

---
+++ src/estructurasDeControl/verificadorEdad.java
@@ -0,0 +1,7 @@
+package estructurasDeControl;
+
+public class verificadorEdad {
+
+    //Crea un programa que verifique si una persona es mayor de edad.
+    //La persona debe ingresar su edad, y el programa debe imprimir si es mayor de edad (18 años o más) o menor de edad.
+}
