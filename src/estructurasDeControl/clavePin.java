65e8425e1a48d98052fa4830dd964f78bde3cd21
drodrigor <danierrodrigo@gmail.com>
2025-04-04 09:02:28 +0200

    Crea un programa que solicite al usuario un PIN de 4 dígitos. Si el PIN es correcto, muestra "Acceso concedido". De lo contrario, muestra "PIN incorrecto, intente de nuevo". Permite 3 intentos antes de bloquear el acceso.

---
+++ src/estructurasDeControl/clavePin.java
@@ -0,0 +1,7 @@
+package estructurasDeControl;
+
+public class clavePin {
+
+    //Crea un programa que solicite al usuario un PIN de 4 dígitos. Si el PIN es correcto, muestra "Acceso concedido".
+    // De lo contrario, muestra "PIN incorrecto, intente de nuevo". Permite 3 intentos antes de bloquear el acceso.
+}
