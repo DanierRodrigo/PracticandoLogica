bdcd7dda9db624f8d48b9d37cc02d34815adab81
drodrigor <danierrodrigo@gmail.com>
2025-04-11 14:57:54 +0200

    sumarElementosArray

--- src/arreglosYcadenas/sumarElementosArray.java
+++ src/arreglosYcadenas/sumarElementosArray.java
@@ -3,4 +3,17 @@ package arreglosYcadenas;
 public class sumarElementosArray {
 
     //Dado un array de números enteros, sumar todos los elementos y devolver el resultado.
+    public static void main(String[] args) {
+
+        //Declaramos las variables
+        int[] numeros = {1, 2, 3, 4, 5};
+        int suma = 0;
+
+        // Sumar todos los elementos del array
+        for (int num : numeros) {
+            suma += num;
+        }
+
+        System.out.println("La suma de los elementos es: " + suma);
+    }
 }
