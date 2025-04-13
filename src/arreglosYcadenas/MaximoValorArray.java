05324a87710a454bc3f741f10af9563e44e4d844
drodrigor <danierrodrigo@gmail.com>
2025-04-13 23:39:47 +0200

    MaximoValorArray

--- src/arreglosYcadenas/MaximoValorArray.java
+++ src/arreglosYcadenas/MaximoValorArray.java
@@ -3,4 +3,17 @@ package arreglosYcadenas;
 public class MaximoValorArray {
 
     //Dado un array de números enteros, encontrar el valor máximo.
+    public static void main(String[] args) {
+        int[] numeros = {15, 22, 8, 41, 33};
+        int max = numeros[0];  // Inicializamos el máximo con el primer elemento
+
+        // Comparar cada elemento con el máximo actual
+        for (int num : numeros) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("El valor máximo es: " + max);
+    }
 }
