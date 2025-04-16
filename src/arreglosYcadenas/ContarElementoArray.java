1394e2d074d357a1d09ef7f171b20941419b332f
drodrigor <danierrodrigo@gmail.com>
2025-04-16 14:38:38 +0200

    ContarElementoArray

--- src/arreglosYcadenas/ContarElementoArray.java
+++ src/arreglosYcadenas/ContarElementoArray.java
@@ -3,4 +3,18 @@ package arreglosYcadenas;
 public class ContarElementoArray {
 
     //Dado un array de números enteros, contar cuántas veces aparece un número específico.
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 3, 5, 3};
+        int buscar = 3;
+        int contador = 0;
+
+        // Contar cuántas veces aparece el número
+        for (int num : numeros) {
+            if (num == buscar) {
+                contador++;
+            }
+        }
+
+        System.out.println("El número " + buscar + " aparece " + contador + " veces.");
+    }
 }
