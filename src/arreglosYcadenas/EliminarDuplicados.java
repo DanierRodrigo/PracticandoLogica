47710ac4662e4a67611f8d6f0e41e27b7abd7475
drodrigor <danierrodrigo@gmail.com>
2025-04-20 00:14:06 +0200

    EliminarDuplicados

--- src/arreglosYcadenas/EliminarDuplicados.java
+++ src/arreglosYcadenas/EliminarDuplicados.java
@@ -1,7 +1,21 @@
 package arreglosYcadenas;
 
+import java.util.HashSet;
+
 public class EliminarDuplicados {
 
     //Dado un array de números enteros,
     //eliminar los elementos duplicados y mostrar el array sin repeticiones.
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 2, 3, 4, 4, 5};
+
+        // Usar un HashSet para eliminar duplicados
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : numeros) {
+            set.add(num);
+        }
+
+        // Mostrar el array sin duplicados
+        System.out.println("Array sin duplicados: " + set);
+    }
 }
