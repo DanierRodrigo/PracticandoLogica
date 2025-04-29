88be0e53069919a2ce3b80e2a6151a3e03d6d59c
drodrigor <danierrodrigo@gmail.com>
2025-04-29 15:16:35 +0200

    Atributos

--- src/pOO/alquilerCoches/Alquiler.java
+++ src/pOO/alquilerCoches/Alquiler.java
@@ -1,6 +1,14 @@
 package pOO.alquilerCoches;
 
+import java.util.Date;
+
+//Representa el vínculo entre cliente y coche
 public class Alquiler {
 
-    //Representa el vínculo entre cliente y coche
+    //Atributos
+    private Cliente cliente;
+    private Coche coche;
+    private Date fechaInicio;
+    private Date fechaFin;
+
 }
