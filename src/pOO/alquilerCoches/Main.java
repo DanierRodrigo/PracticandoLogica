c4190e85d8545a88dbd59263a28fbbe950a31dba
drodrigor <danierrodrigo@gmail.com>
2025-05-14 10:40:39 +0200

    Agregamos coches

--- src/pOO/alquilerCoches/Main.java
+++ src/pOO/alquilerCoches/Main.java
@@ -1,4 +1,16 @@
 package pOO.alquilerCoches;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //Iniciamos GestionAlquiler
+        GestionAlquiler gestion = new GestionAlquiler();
+
+        //Agregamos coches
+        gestion.agregarCoche(new Coche("Toyota", "Corolla", 2020, true));
+        gestion.agregarCoche(new Coche("Honda", "Civic", 2019, true));
+        gestion.agregarCoche(new Coche("Ford", "Fiesta", 2022, true));
+        gestion.agregarCoche(new Coche("Porche", "Carrera GT", 2005, true));
+    }
 }
