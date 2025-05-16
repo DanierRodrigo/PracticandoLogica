b102971c393ad1d20e7bb30618911daa394157cc
drodrigor <danierrodrigo@gmail.com>
2025-05-16 10:12:11 +0200

    Creación y agregación de coches al sistema

--- src/pOO/alquilerCoches/Main.java
+++ src/pOO/alquilerCoches/Main.java
@@ -16,11 +16,18 @@ public class Main {
         // Crear instancia de gestión
         GestionAlquiler gestion = new GestionAlquiler(listaAlquileres, listaCoches);
 
-        //Agregamos coches
+        //Creamos coches y los agregamos directamente o lo hacemos por separado
         gestion.agregarCoche(new Coche("Toyota", "Corolla", 2020, true));
         gestion.agregarCoche(new Coche("Honda", "Civic", 2019, true));
         gestion.agregarCoche(new Coche("Ford", "Fiesta", 2022, true));
         gestion.agregarCoche(new Coche("Porche", "Carrera GT", 2005, true));
+
+        Coche coche1 = new Coche("Ferrari", "Roma", 2024, true);
+        Coche coche2 = new Coche("Maserati", "Ghibli", 2025, true);
+
+        gestion.agregarCoche(coche1);
+        gestion.agregarCoche(coche2);
+
 
         //Creamos cliente
         Cliente cliente1 = new Cliente("Daniel Rodrigo", "28826162", "638254112");
