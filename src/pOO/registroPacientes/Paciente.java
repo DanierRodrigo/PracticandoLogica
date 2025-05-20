e6e0c59f50f42b12b25a7db96926bd52ba2cb8cd
drodrigor <danierrodrigo@gmail.com>
2025-05-20 17:14:37 +0200

    Atributos

--- src/pOO/registroPacientes/Paciente.java
+++ src/pOO/registroPacientes/Paciente.java
@@ -1,4 +1,11 @@
 package pOO.registroPacientes;
 
+import java.util.List;
+
 public class Paciente {
+
+    //Atributos
+    private String nombre;
+    private int edad;
+    private List<String> historial;
 }
