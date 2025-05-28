98d259593fb0b7dc8c2a9ee450510235260cc5f8
drodrigor <danierrodrigo@gmail.com>
2025-05-28 08:23:41 +0200

    Atributos

--- src/pOO/registroPacientes/Consulta.java
+++ src/pOO/registroPacientes/Consulta.java
@@ -1,4 +1,12 @@
 package pOO.registroPacientes;
 
+import java.time.LocalDate;
+
 public class Consulta {
+
+    //Atributos
+    private Paciente paciente;
+    private Doctor doctor;
+    private LocalDate fecha;
+    private String diagnostico;
 }
