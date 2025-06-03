24111144561b02c2778dd2f1c71a35850d77f467
drodrigor <danierrodrigo@gmail.com>
2025-06-03 11:57:53 +0200

    Crear doctores

--- src/pOO/registroPacientes/SistemaGestionPacientes.java
+++ src/pOO/registroPacientes/SistemaGestionPacientes.java
@@ -1,4 +1,11 @@
 package pOO.registroPacientes;
 
 public class SistemaGestionPacientes {
+    public static void main(String[] args) {
+
+        // Crear doctores
+        Doctor drRodrigo = new Doctor("Dr. Rodrigo", "Traumatología");
+        Doctor draBucas = new Doctor("Dra. Bucas", "Pediatría");
+        Doctor drCalero = new Doctor("Dr. Calero", "Fisioterapia");
+    }
 }
