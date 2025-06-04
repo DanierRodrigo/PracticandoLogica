5fb454012453acb820eab4dc7841a470391ad011
drodrigor <danierrodrigo@gmail.com>
2025-06-04 08:57:11 +0200

    Crear pacientes

--- src/pOO/registroPacientes/SistemaGestionPacientes.java
+++ src/pOO/registroPacientes/SistemaGestionPacientes.java
@@ -7,5 +7,10 @@ public class SistemaGestionPacientes {
         Doctor drRodrigo = new Doctor("Dr. Rodrigo", "Traumatología");
         Doctor draBucas = new Doctor("Dra. Bucas", "Pediatría");
         Doctor drCalero = new Doctor("Dr. Calero", "Fisioterapia");
+
+        // Crear pacientes
+        Paciente juan = new Paciente("Juan Pérez", 45);
+        Paciente maria = new Paciente("María López", 12);
+        Paciente miguel = new Paciente("Miguel Manzano", 22);
     }
 }
