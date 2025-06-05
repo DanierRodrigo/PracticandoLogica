aa14e24cb27c11626ac5d84064b56717e1f4610e
drodrigor <danierrodrigo@gmail.com>
2025-06-05 16:07:59 +0200

    Crear consultas

--- src/pOO/registroPacientes/SistemaGestionPacientes.java
+++ src/pOO/registroPacientes/SistemaGestionPacientes.java
@@ -1,5 +1,9 @@
 package pOO.registroPacientes;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaGestionPacientes {
     public static void main(String[] args) {
 
@@ -12,5 +16,11 @@ public class SistemaGestionPacientes {
         Paciente juan = new Paciente("Juan Pérez", 45);
         Paciente maria = new Paciente("María López", 12);
         Paciente miguel = new Paciente("Miguel Manzano", 22);
+
+        // Registrar consultas
+        List<Consulta> consultas = new ArrayList<>();
+        consultas.add(new Consulta(juan, drRodrigo, LocalDate.of(2025, 5, 20), "Traumatismo craneoencefálico"));
+        consultas.add(new Consulta(maria, draBucas, LocalDate.of(2025, 5, 19), "Gripe"));
+        consultas.add(new Consulta(maria, drCalero, LocalDate.of(2025, 5, 19), "Rehabilitación de hombro"));
     }
 }
