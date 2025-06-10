cf341267c44a2c2b131f6176ec2f8829afc4f5ee
drodrigor <danierrodrigo@gmail.com>
2025-06-10 09:20:02 +0200

    Atributos

--- src/pOO/controlJuegos/Juego.java
+++ src/pOO/controlJuegos/Juego.java
@@ -1,4 +1,10 @@
 package pOO.controlJuegos;
 
 public class Juego {
+
+    //Atributos
+    private String nombre;
+    private String plataforma;
+    private int anioCreacion;
+    private String desarrolladora;
 }
