884abe8f2f1b57f899b9c3718918ca01eff4d584
drodrigor <danierrodrigo@gmail.com>
2025-06-25 08:34:44 +0200

    Método ColeccionJuegos()

--- src/pOO/controlJuegos/ColeccionJuegos.java
+++ src/pOO/controlJuegos/ColeccionJuegos.java
@@ -1,9 +1,15 @@
 package pOO.controlJuegos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ColeccionJuegos {
 
     //Atributos
     private List<Juego> juegos;
+
+    //Métodos
+    public ColeccionJuegos() {
+        this.juegos = new ArrayList<>();
+    }
 }
