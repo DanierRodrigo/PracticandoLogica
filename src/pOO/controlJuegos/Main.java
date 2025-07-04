63defe1b0881e6a61eb174ceab0e49ef47139894
drodrigor <danierrodrigo@gmail.com>
2025-07-04 09:07:17 +0200

    Creando juego1

--- src/pOO/controlJuegos/Main.java
+++ src/pOO/controlJuegos/Main.java
@@ -4,4 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ColeccionJuegos coleccion = new ColeccionJuegos();
     }
+    //Creación de juegos
+    Juego juego1 = new Juego("The Legend of Zelda", "Nintendo Switch", 2017, "Nintendo");
 }
