a103ef030a6fe1ac8511653dd2a2c5c3f50e06fb
drodrigor <danierrodrigo@gmail.com>
2025-07-11 08:46:20 +0200

    Creando juego5

--- src/pOO/controlJuegos/Main.java
+++ src/pOO/controlJuegos/Main.java
@@ -10,4 +10,8 @@ public class Main {
     Juego juego2 = new Juego("God of War", "PlayStation 4", 2018, "Santa Monica Studio");
 
     Juego juego3 = new Juego("Donkey Kong Land", "Game Boy", 1995, "Rare");
+
+    Juego juego4 = new Juego("Wave Race", "Nintendo 64", 1997, "Nintendo");
+
+    Juego juego5 = new Juego("The legend of Zelda Twilight Princess", "Nintendo Game Cube", 2005, "Nintendo");
 }
