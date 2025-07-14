708511d0a6c942f1a1354ab35ae84c100bdf79a7
drodrigor <danierrodrigo@gmail.com>
2025-07-14 14:40:50 +0200

    Creando juego5

--- src/pOO/controlJuegos/Main.java
+++ src/pOO/controlJuegos/Main.java
@@ -12,4 +12,6 @@ public class Main {
     Juego juego3 = new Juego("Donkey Kong Land", "Game Boy", 1995, "Rare");
 
     Juego juego4 = new Juego("Wave Race", "Nintendo 64", 1997, "Nintendo");
+
+    Juego juego5 = new Juego("Metroid Prime", "Nintendo Game Cube", 2002, "Nintendo");
 }
